fc7b2cceb04f6df51ba03e346b2e6e38c052e41b
@gncrysr <gncrysr@gmail.com>
2023-01-07 15:33:36 +0300

    minor update on basic melee visual

--- NextRogue/Assets/Game/Scripts/Player/Attack/APlayerBasicProjectile.cs
+++ NextRogue/Assets/Game/Scripts/Player/Attack/APlayerBasicProjectile.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public abstract class APlayerBasicProjectile : MonoBehaviour
 {
     Vector3 _mousePos;
-    [SerializeField] GameObject _main;
     public virtual void Initialize(Vector3 mousePos) {
-        Destroy(_main, 4f);
+        Destroy(gameObject, 4f);
 
         _mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-        SetRotation(_main.transform);
+        SetRotation(gameObject.transform);
     }
     void SetRotation(Transform target) {
         _mousePos.z = 0f;
