b19bf05e05f9c3da052c8f54e78c6f19e82f93b5
YasarGencer <gncrysr@gmail.com>
2023-10-22 16:58:47 +0300

    Boss Comic Scene and Necromancer Reaper Fix

--- NextRogue/Assets/Game/Scripts/Spells/Projectiles/NecromancerReaperProjectile.cs
+++ NextRogue/Assets/Game/Scripts/Spells/Projectiles/NecromancerReaperProjectile.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
 
 public class NecromancerReaperProjectile : AP_Projectile {
+    Transform player;
     public override void Initialize(Vector3 mousePos, float damage, float time, float speed, DOTInfo dotInfo) {
         base.Initialize(mousePos, damage, time * 2, speed, dotInfo);
+        player = MainManager.Instance.Player.GetChild(0);
+    }
+    private void Update() {
+        transform.position = player.position;
     }
 }
