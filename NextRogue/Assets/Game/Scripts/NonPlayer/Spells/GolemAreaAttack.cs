8253bed0b8289224e8eb951efcae20f3b6c8eef3
kayrakocaeli <83376559+kayrakocaeli@users.noreply.github.com>
2023-10-23 11:46:17 +0300

    Vampire-Melee and Golem(execpt skill)

---
+++ NextRogue/Assets/Game/Scripts/NonPlayer/Spells/GolemAreaAttack.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.ParticleSystem;
+
+[CreateAssetMenu(fileName = "GolemAreaAttack", menuName = "ScriptableObjects/EnemySpells/GolemAreaAttack")]
+
+public class GolemAreaAttack : ANP_Spell
+{
+
+    public GameObject Particle;
+
+    public override bool CheckConditions(ANP_MainController mainController)
+    {
+        if (base.CheckConditions(mainController) == false)
+            return false;
+        if (_mainController.Distance(_mainController.Target.Target.transform) > UseRange)
+            return false;
+        return true;
+
+    }
+    public override void ActivateSpell()
+    {
+        base.ActivateSpell();
+
+    }
+    
+
+}
