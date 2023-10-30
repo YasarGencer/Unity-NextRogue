247d239b8e8089112bcf950a283ae68ae1df95fe
Batuhan Erdin <ejderfora@outlook.com.tr>
2023-10-30 19:46:35 +0300

    Golem npc added , golem skill coded ,near finish

--- NextRogue/Assets/Game/Scripts/NonPlayer/Spells/GolemAreaAttack.cs
+++ NextRogue/Assets/Game/Scripts/NonPlayer/Spells/GolemAreaAttack.cs
@@ -1,15 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 using static UnityEngine.ParticleSystem;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 [CreateAssetMenu(fileName = "GolemAreaAttack", menuName = "ScriptableObjects/EnemySpells/GolemAreaAttack")]
 
 public class GolemAreaAttack : ANP_Spell
 {
 
-    public GameObject Particle;
-
+   
+    IDisposable _moveRX;
+    public override void Initialize(ANP_MainController mainController)
+    {
+        base.Initialize(mainController);
+    }
     public override bool CheckConditions(ANP_MainController mainController)
     {
         if (base.CheckConditions(mainController) == false)
@@ -22,12 +29,7 @@ public class GolemAreaAttack : ANP_Spell
     public override void ActivateSpell()
     {
         base.ActivateSpell();
-
+        Instantiate(Spell, _mainController.transform).GetComponent<AProjectile>().Initialize(_mainController.transform.position,Damage,CooldownTime,Speed,DOTInfo);
     }
-    void StoneSmash()
-    {
-       // Collider[] colliders = Physics.OverlapSphere();
-    }
-    
-
+   
 }
---
+++ NextRogue/Assets/Game/Scripts/NonPlayer/Spells/Projectiles/GroundSlam_Projectile.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using Cinemachine.Utility;
+
+public class GroundSlam_Projectile : ANP_Projectile
+{
+    public GameObject Particle;
+
+    private SpriteRenderer maxAttackSprite;
+    private SpriteRenderer currentAttackSprite;
+    private Vector3 oldScale;
+    private bool smashing;
+
+    private float duration = 2f;
+    private float timer = 0f;
+    private bool soundPlayed;
+
+    IDisposable _moveRX;
+    public override void Initialize(Vector3 targetPos, float damage, float time, float speed, DOTInfo dotInfo)
+    {
+        base.Initialize(targetPos, damage, time, speed, dotInfo);
+        //base.Initialize(mainController);
+
+        _moveRX?.Dispose();
+
+        if (maxAttackSprite == null)
+            maxAttackSprite = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        if (currentAttackSprite == null)
+            currentAttackSprite = transform.GetChild(1).GetComponent<SpriteRenderer>();
+        currentAttackSprite.enabled = false;
+        maxAttackSprite.enabled = false;
+        oldScale = currentAttackSprite.transform.localScale;
+
+        _moveRX = Observable.EveryUpdate().TakeUntilDisable(this).Subscribe(MoveRX);
+        smashing = true;
+    }
+    public void MoveRX(long obj)
+    {
+        StoneSmash();
+    }
+    void StoneSmash()
+    {
+        currentAttackSprite.enabled = true;
+        maxAttackSprite.enabled = true;
+        if (smashing == true)
+        {
+            timer += Time.deltaTime;
+            float t = timer / duration;
+            currentAttackSprite.transform.localScale = Vector3.Lerp(oldScale, maxAttackSprite.transform.localScale, t);
+
+            if (timer >= 2 && soundPlayed == false)
+            {
+                PlaySound();
+                //AudioManager.PlaySound(Sound, _mainController.transform, AudioManager.AudioVolume.sfx, stopAudio);
+                soundPlayed = true;
+            }
+            if (timer >= duration)
+            {
+                timer = 0f;
+                currentAttackSprite.transform.localScale = oldScale;
+                currentAttackSprite.enabled = false;
+                maxAttackSprite.enabled = false;
+                smashing = false;
+                gameObject.GetComponent<CircleCollider2D>().enabled = true;
+                gameObject.GetComponent<Damager>().Initialize(_damage,_dotInfo);  
+                _moveRX?.Dispose();
+                //buraya golem playerin colliderýnda ise hasar ver knocpback at
+            }
+        }
+    }
+    protected override void OnGamePause()
+    {
+        base.OnGamePause();
+        _moveRX?.Dispose();
+    }
+    protected override void OnGameUnPause()
+    {
+        base.OnGameUnPause();
+        _moveRX = Observable.EveryUpdate().TakeUntilDisable(this).Subscribe(MoveRX);
+    }
+}
