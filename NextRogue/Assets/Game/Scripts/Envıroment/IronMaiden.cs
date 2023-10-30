b517fd5448bc814848f3a59fb4ab0b1c4746089d
YasarGencer <gncrysr@gmail.com>
2023-10-30 21:22:48 +0300

    Bloodshapers Dungeon Update

--- NextRogue/Assets/Game/Scripts/Envıroment/IronMaiden.cs
+++ NextRogue/Assets/Game/Scripts/Envıroment/IronMaiden.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System; 
 using System.Threading.Tasks;
 using UniRx;
@@ -8,6 +9,7 @@ public class IronMaiden : MonoBehaviour {
     [SerializeField] GameObject projectile;
     [SerializeField] float stayClosed;
     [SerializeField] float stayOpen;
+    [SerializeField] GameObject opened, closed;
     bool isOpen = false;
     float activeTimer = 0;
     Animator animator;
@@ -18,10 +20,13 @@ public class IronMaiden : MonoBehaviour {
         MainManager.Instance.EventManager.onGameUnPause += UnPause;
         animator = GetComponent<Animator>(); 
         UnPause();
+        opened.SetActive(false);
+        closed.SetActive(true);
     }
     private void UpdateRX(long obj) {
         activeTimer -= Time.deltaTime;
-        if (activeTimer < 0) { 
+        if (activeTimer < 0) {
+            transform.DOPunchScale(Vector3.one * .5f, .4f); 
             if (isOpen)
                 Close();
             else
@@ -29,19 +34,21 @@ public class IronMaiden : MonoBehaviour {
         } 
     }
     async void Open() {
-        Debug.Log("actim");
+        opened.SetActive(true);
+        closed.SetActive(false); 
         isOpen = true;
         activeTimer = stayOpen;
         animator.SetTrigger("Open");
-        await Task.Delay(500);
-        Debug.Log("ates");
-        Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<AProjectile>().Initialize(Vector3.zero, damage, 1f, 150, new DOTInfo(DOTType.BLEED, 0, 0));
+        //await Task.Delay(500); 
+        Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<AProjectile>().Initialize(Vector3.zero, damage, 1.5f, 150, new DOTInfo(DOTType.BLEED, 0, 0));
+
     }
     void Close() {
-        Debug.Log("kapadim");
-        animator.SetTrigger("Close");
-        isOpen = false;
+        opened.SetActive(false);
+        closed.SetActive(true);
         activeTimer = stayClosed;
+        isOpen = false;
+        animator.SetTrigger("Close");
     }
     private void OnDestroy() {
         MainManager.Instance.EventManager.onGamePause -= Pause;
